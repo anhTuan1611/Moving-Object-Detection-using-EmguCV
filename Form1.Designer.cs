a91afcf114903654cf447c176727cedcb3cb4f12
Nguyen Anh Tuan <163486233+anhTuan1611@users.noreply.github.com>
2024-05-10 18:13:54 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace code_detect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeVideoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cameraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectCameraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.MistyRose;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1,
+            this.fileToolStripMenuItem,
+            this.cameraToolStripMenuItem,
+            this.optionToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(101, 24);
+            this.fileToolStripMenuItem1.Text = "Information";
+            this.fileToolStripMenuItem1.Click += new System.EventHandler(this.fileToolStripMenuItem1_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeVideoToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.fileToolStripMenuItem.Text = "Video";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "OpenVideo";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeVideoToolStripMenuItem
+            // 
+            this.closeVideoToolStripMenuItem.Name = "closeVideoToolStripMenuItem";
+            this.closeVideoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.closeVideoToolStripMenuItem.Text = "CloseVideo";
+            this.closeVideoToolStripMenuItem.Click += new System.EventHandler(this.closeVideoToolStripMenuItem_Click);
+            // 
+            // cameraToolStripMenuItem
+            // 
+            this.cameraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5});
+            this.cameraToolStripMenuItem.Name = "cameraToolStripMenuItem";
+            this.cameraToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.cameraToolStripMenuItem.Text = "Camera";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(179, 26);
+            this.toolStripMenuItem4.Text = "OpenCamera";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(179, 26);
+            this.toolStripMenuItem5.Text = "CloseCamera";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // optionToolStripMenuItem
+            // 
+            this.optionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detectToolStripMenuItem,
+            this.detectCameraToolStripMenuItem});
+            this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(115, 24);
+            this.optionToolStripMenuItem.Text = "Detect Object";
+            this.optionToolStripMenuItem.Click += new System.EventHandler(this.optionToolStripMenuItem_Click);
+            // 
+            // detectToolStripMenuItem
+            // 
+            this.detectToolStripMenuItem.Name = "detectToolStripMenuItem";
+            this.detectToolStripMenuItem.Size = new System.Drawing.Size(187, 26);
+            this.detectToolStripMenuItem.Text = "DetectVideo";
+            this.detectToolStripMenuItem.Click += new System.EventHandler(this.detectToolStripMenuItem_Click);
+            // 
+            // detectCameraToolStripMenuItem
+            // 
+            this.detectCameraToolStripMenuItem.Name = "detectCameraToolStripMenuItem";
+            this.detectCameraToolStripMenuItem.Size = new System.Drawing.Size(187, 26);
+            this.detectCameraToolStripMenuItem.Text = "DetectCamera";
+            this.detectCameraToolStripMenuItem.Click += new System.EventHandler(this.detectCameraToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 393);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.RosyBrown;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 393);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Moving Object Detection";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem detectCameraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeVideoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cameraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+
+
+
+
+namespace code_detect
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        VideoCapture cameraCapture;
+        
+        Mat frame;
+        Mat foregroundMask;
+       
+        bool captureInProgress;
+
+        IBackgroundSubtractor backgroundSubtractor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Moving Object Detection on VIDEO
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat frame = capture.QueryFrame();
+                if (frame.IsEmpty)
+                {
+                    Application.Idle -= Application_Idle;
+                    return;
+                }
+
+                Mat smoothFrame = new Mat();
+                CvInvoke.GaussianBlur(frame, smoothFrame, new Size(3, 3), 1);
+
+                Mat foregroundMask = new Mat();
+                backgroundSubtractor.Apply(smoothFrame, foregroundMask);
+
+
+                CvInvoke.Threshold(foregroundMask, foregroundMask, 200, 240, ThresholdType.Binary);
+                CvInvoke.MorphologyEx(foregroundMask, foregroundMask, MorphOp.Close,
+                    Mat.Ones(7, 3, DepthType.Cv8U, 1), new Point(-1, -1), 1, BorderType.Reflect, new MCvScalar(0));
+
+                int minArea = 500;
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                CvInvoke.FindContours(foregroundMask, contours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    var bbox = CvInvoke.BoundingRectangle(contours[i]);
+                    var area = bbox.Width * bbox.Height;
+                    var ar = (float)bbox.Width / bbox.Height;
+
+                    if (area > minArea && ar < 1.0)
+                    {
+                        CvInvoke.Rectangle(frame, bbox, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                }
+                pictureBox1.Image = frame.ToBitmap();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Video Files (*.mp4;*.avi;)|*.mp4;*.avi;";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    capture = new VideoCapture(dialog.FileName);
+                    if (capture != null)
+                    {
+                        Mat frame = capture.QueryFrame(); // Đọc khung đầu tiên
+                        if (!frame.IsEmpty)
+                            pictureBox1.Image = frame.ToBitmap(); // Hiển thị nó
+                        else
+                            MessageBox.Show("Cannot load frame.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void closeVideoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            if (capture != null && capture.IsOpened)
+            {
+                capture.Stop(); 
+                capture.Dispose();
+                capture = null;
+                pictureBox1.Image = null; 
+                MessageBox.Show("Video has been stopped.");
+
+
+            }
+            else
+            {
+                MessageBox.Show("Video is not running.");
+            }
+        }
+
+        private void detectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (capture != null && !capture.Ptr.Equals(IntPtr.Zero))
+                {
+                    backgroundSubtractor = new BackgroundSubtractorMOG2();
+                   
+                    Application.Idle += Application_Idle; // Bắt đầu xử lý mỗi khung hình mới
+                    
+
+                }
+                else
+                {
+                    MessageBox.Show("Please open a video first.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat frame = new Mat();
+                if (cameraCapture != null && cameraCapture.Ptr != IntPtr.Zero)
+                {
+                    cameraCapture.Retrieve(frame, 0);
+                    if (!frame.IsEmpty)
+                        pictureBox1.Image = frame.ToBitmap(); // For example, show the frame in a PictureBox
+                    else
+                        MessageBox.Show("Cannot read frame from camera.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        //Cai dat them nut nhan
+        private void optionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to end the program?", "Exit Confirmation", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+        //Mo camera va detect moving objection
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            cameraCapture = new VideoCapture(0); // Mở camera mặc định
+            cameraCapture.ImageGrabbed += ProcessFrame;
+            cameraCapture.Start();
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem cameraCapture đã được khởi tạo và đã bắt đầu chưa
+            if (cameraCapture != null && cameraCapture.IsOpened)
+            {
+                cameraCapture.Stop(); // Dừng camera
+                cameraCapture.Dispose(); // Giải phóng tài nguyên
+                cameraCapture = null;
+                pictureBox1.Image = null; // Xóa ảnh hiển thị
+                MessageBox.Show("Camera has been stopped.");
+
+
+            }
+            else
+            {
+                MessageBox.Show("Camera is not running.");
+            }
+        }
+        private void DetectMotion(Mat frame)
+        {
+            using (Mat smoothFrame = new Mat())
+            {
+                CvInvoke.GaussianBlur(frame, smoothFrame, new Size(3, 3), 1);
+                using (Mat foregroundMask = new Mat())
+                {
+                    backgroundSubtractor.Apply(smoothFrame, foregroundMask);
+
+                    // ...các bước xử lý khác...
+
+                    VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                    CvInvoke.FindContours(foregroundMask, contours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+                    for (int i = 0; i < contours.Size; i++)
+                    {
+                        var bbox = CvInvoke.BoundingRectangle(contours[i]);
+                        var area = bbox.Width * bbox.Height; // Tính diện tích của bbox
+
+                        if (area > 500) // Kiểm tra diện tích
+                        {
+                            CvInvoke.Rectangle(frame, bbox, new MCvScalar(0, 0, 255), 2);
+                        }
+                    }
+                    pictureBox1.Image = frame.ToBitmap(); // Hiển thị frame đã qua xử lý
+                }
+            }
+        }
+        private void Application_Idle1(object sender, EventArgs e)
+        {
+            try
+            {
+                using (Mat frame = new Mat())
+                {
+                    cameraCapture.Retrieve(frame, 0);
+                    if (!frame.IsEmpty)
+                    {
+                        DetectMotion(frame);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void detectCameraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (cameraCapture != null && cameraCapture.IsOpened)
+            {
+                backgroundSubtractor = new BackgroundSubtractorMOG2();
+                Application.Idle += Application_Idle1; // Bắt đầu xử lý mỗi khung hình mới
+            }
+            else
+            {
+                MessageBox.Show("Please open the camera first.");
+            }
+        }
+        //Hiển thị thông tin cá nhân
+       
+        private void fileToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nguyễn Anh Tuấn - 21146570");
+
+        }
+    }
+}
